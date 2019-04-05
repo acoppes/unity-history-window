9534ae14b5e4503b5c4c2fb5dd58492514f03c93
Ariel Coppes <ariel.coppes@gmail.com>
2019-04-04 22:39:30 -0300

    moved to folder Gemserk.SelectionHistory

--- Assets/Editor/ExportPackageAction.cs
+++ Assets/Editor/ExportPackageAction.cs
@@ -9,9 +9,22 @@ namespace Gemserk.Tools
         [MenuItem("Assets/Gemserk/Export Package")]
         public static void ExportPackage()
         {
-            var rootPath = Application.dataPath.Replace("/Assets", "");
-            var packageFile = Path.Combine(rootPath, "release/unity-selection-history.unitypackage");
-            AssetDatabase.ExportPackage("Assets/Gemserk", packageFile, ExportPackageOptions.Recurse);
+            try
+            {
+                EditorUtility.DisplayProgressBar("Exporting package", "", 0.5f);
+
+                var rootPath = Application.dataPath.Replace("/Assets", "");
+                var packageFile = Path.Combine(rootPath, "release/unity-selection-history.unitypackage");
+                AssetDatabase.ExportPackage("Assets/Gemserk.SelectionHistory", packageFile,
+                    ExportPackageOptions.Recurse);
+                
+                EditorUtility.DisplayProgressBar("Exporting package", "", 1);
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+            }
+            
         }
     }
 }
