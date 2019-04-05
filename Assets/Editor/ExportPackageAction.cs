deebda2e8fab5befe2f20604ac734898c11cb4a6
Ariel Coppes <ariel.coppes@gmail.com>
2019-04-04 22:26:06 -0300

    menu action to export package to release folder

---
+++ Assets/Editor/ExportPackageAction.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Gemserk.Tools
+{
+    public static class ExportPackageAction
+    {
+        [MenuItem("Assets/Gemserk/Export Package")]
+        public static void ExportPackage()
+        {
+            var rootPath = Application.dataPath.Replace("/Assets", "");
+            var packageFile = Path.Combine(rootPath, "release/unity-selection-history.unitypackage");
+            AssetDatabase.ExportPackage("Assets/Gemserk", packageFile, ExportPackageOptions.Recurse);
+        }
+    }
+}
