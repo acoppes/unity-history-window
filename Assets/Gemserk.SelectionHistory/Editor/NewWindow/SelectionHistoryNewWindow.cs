1153284fd8992c68fb82b91297acfdcce9883c39
Ariel Coppes <ariel.coppes@gmail.com>
2019-05-19 20:54:58 -0300

    fixed css load

--- Assets/Gemserk.SelectionHistory/Editor/NewWindow/SelectionHistoryNewWindow.cs
+++ Assets/Gemserk.SelectionHistory/Editor/NewWindow/SelectionHistoryNewWindow.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 
-
 public class SelectionHistoryNewWindow : EditorWindow
 {
-    [MenuItem("Window/UIElements/SelectionHistoryNewWindow")]
+    private static readonly string StyleSheetFileName = "SelectionHistoryNewWindow";
+    
+    private static Vector2 _windowMinSize = new Vector2(300, 200);
+
+    [MenuItem("Window/Gemserk/New Selection History")]
     public static void ShowExample()
     {
         var window = GetWindow<SelectionHistoryNewWindow>();
         window.titleContent = new GUIContent("SelectionHistoryNewWindow");
+        window.minSize = _windowMinSize;
     }
 
     public void OnEnable()
@@ -22,14 +26,16 @@ public class SelectionHistoryNewWindow : EditorWindow
         VisualElement label = new Label("Hello World! From C#");
         root.Add(label);
 
-        // Import UXML
-      //  var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Gemserk.SelectionHistory/Editor/SelectionHistoryNewWindow.uxml");
-       // VisualElement labelFromUXML = visualTree.CloneTree();
-       // root.Add(labelFromUXML);
+        var styleSheetGuids = AssetDatabase.FindAssets("t:StyleSheet " + StyleSheetFileName);
 
-        // A stylesheet can be added to a VisualElement.
-        // The style will be applied to the VisualElement and all of its children.
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Gemserk.SelectionHistory/Editor/SelectionHistoryNewWindow.uss");
+        if (styleSheetGuids.Length == 0)
+        {
+            Debug.LogError("Failed to find selection history style sheet");
+            return;
+        }
+
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(AssetDatabase.GUIDToAssetPath(styleSheetGuids[0]));
+        // var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Gemserk.SelectionHistory/Editor/SelectionHistoryNewWindow.uss");
         VisualElement labelWithStyle = new Label("Hello World! With Style");
         labelWithStyle.styleSheets.Add(styleSheet);
         root.Add(labelWithStyle);
