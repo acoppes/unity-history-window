f0de5e59eaa12aaff6596b9b4f8045960379f1f8
Ariel Coppes <ariel.coppes@gmail.com>
2019-05-25 20:10:36 -0300

    just some tests with object drag

--- Assets/Gemserk.SelectionHistory/Editor/NewWindow/SelectionHistoryNewWindow.cs
+++ Assets/Gemserk.SelectionHistory/Editor/NewWindow/SelectionHistoryNewWindow.cs
@@ -1,5 +1,3 @@
-using System;
-using Gemserk;
 using UnityEditor;
 using UnityEditor.ShortcutManagement;
 using UnityEngine;
@@ -7,127 +5,135 @@ using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 using Object = UnityEngine.Object;
 
-public class SelectionHistoryNewWindow : EditorWindow
+namespace Gemserk.Editor
 {
-    private static readonly string StyleSheetFileName = "SelectionHistoryNewWindow";
-    private static readonly string VisualTreeFileName = "SelectionHistoryNewWindow";
-    
-    private static Vector2 _windowMinSize = new Vector2(300, 200);
-
-    [Shortcut("Selection History/Show", null, KeyCode.H, ShortcutModifiers.Action | ShortcutModifiers.Shift)]
-    [MenuItem("Window/Gemserk/New Selection History")]
-    public static void OpenSelectionHistoryWindow()
+    public class SelectionHistoryNewWindow : EditorWindow
     {
-        var window = GetWindow<SelectionHistoryNewWindow>();
-        window.titleContent = new GUIContent("SelectionHistoryNewWindow");
-        window.minSize = _windowMinSize;
-    }
-    
-    private static SelectionHistory selectionHistory
-    {
-        get { return SelectionHistoryContext.SelectionHistory; }
-    }
-
-    private StyleSheet _styleSheet;
-
-    private VisualTreeAsset _visualTreeAsset;
-
-    private VisualElement _historyObjectsContainer;
-
-    private static StyleSheet LoadStyleSheet()
-    {
-        var guids = AssetDatabase.FindAssets("t:StyleSheet " + StyleSheetFileName);
-
-        if (guids.Length == 0)
-            return null;
+        private static readonly string StyleSheetFileName = "SelectionHistoryNewWindow";
+        private static readonly string VisualTreeFileName = "SelectionHistoryNewWindow";
         
-        return AssetDatabase.LoadAssetAtPath<StyleSheet>(AssetDatabase.GUIDToAssetPath(guids[0]));
-    }
+        private static Vector2 _windowMinSize = new Vector2(300, 200);
 
-    private static VisualTreeAsset LoadTreeAsset()
-    {
-        var guids = AssetDatabase.FindAssets("t:VisualTreeAsset " + VisualTreeFileName);
-
-        if (guids.Length == 0)
-            return null;
-
-        return AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(AssetDatabase.GUIDToAssetPath(guids[0]));
-    }
-    
-    public void OnEnable()
-    {
-        // Each editor window contains a root VisualElement object
-        var root = rootVisualElement;
-
-        _historyObjectsContainer = new VisualElement();
-        
-        root.Add(_historyObjectsContainer);
-
-        _styleSheet = LoadStyleSheet();
-        _visualTreeAsset = LoadTreeAsset();
-
-        if (_styleSheet == null || _visualTreeAsset == null)
+        [Shortcut("Selection History/Show", null, KeyCode.H, ShortcutModifiers.Action | ShortcutModifiers.Shift)]
+        [MenuItem("Window/Gemserk/New Selection History")]
+        public static void OpenSelectionHistoryWindow()
         {
-            Debug.LogError("Failed to initialize selection history");
-            return;
+            var window = GetWindow<SelectionHistoryNewWindow>();
+            window.titleContent = new GUIContent("SelectionHistoryNewWindow");
+            window.minSize = _windowMinSize;
         }
         
-        root.styleSheets.Add(_styleSheet);
-        
-        var clearButton = new Button(delegate
+        private static SelectionHistory selectionHistory
         {
-            selectionHistory.Clear();
-            // _historyObjectsContainer.Clear();
-            // clear list too
-        });
-        clearButton.text = "Clear";
-        
-        root.Add(clearButton);
-        
-        var scheduledAction = root.schedule.Execute(() =>
+            get { return SelectionHistoryContext.SelectionHistory; }
+        }
+
+        private StyleSheet _styleSheet;
+
+        private VisualTreeAsset _visualTreeAsset;
+
+        private VisualElement _historyObjectsContainer;
+
+        private static StyleSheet LoadStyleSheet()
         {
-            // textFields.ForEach(t => t.value = m_Tank.tankName);
-            // integerFields.ForEach(t => t.value = m_Tank.tankSize);
-        });
-        scheduledAction.Every(100); // ms
+            var guids = AssetDatabase.FindAssets("t:StyleSheet " + StyleSheetFileName);
 
-        selectionHistory.objectAdded += AddSelectionField;
-        selectionHistory.cleared += () =>
+            if (guids.Length == 0)
+                return null;
+            
+            return AssetDatabase.LoadAssetAtPath<StyleSheet>(AssetDatabase.GUIDToAssetPath(guids[0]));
+        }
+
+        private static VisualTreeAsset LoadTreeAsset()
         {
-            _historyObjectsContainer.Clear();
-        };
+            var guids = AssetDatabase.FindAssets("t:VisualTreeAsset " + VisualTreeFileName);
 
-        // Selection.selectionChanged += OnSelectionChanged;
+            if (guids.Length == 0)
+                return null;
+
+            return AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(AssetDatabase.GUIDToAssetPath(guids[0]));
+        }
         
-        // regenerate
-//        _historyObjectsContainer.Clear();
-        
-        selectionHistory.History.ForEach(AddSelectionField);
-    }
-
-    public void OnDisable()
-    {
-        selectionHistory.objectAdded -= AddSelectionField;
-        // Selection.selectionChanged -= OnSelectionChanged;
-    }
-
-    private void AddSelectionField(Object objectAdded)
-    {
-        var tree = _visualTreeAsset.CloneTree();
-        var selectionContainer = tree.Q("Selection");
-        var objectField = selectionContainer.Q<ObjectField>( );
-
-        var button = selectionContainer.Q<Button>();
-        button.text = "Ping";
-        
-        button.clickable.clicked += delegate
+        public void OnEnable()
         {
-            EditorGUIUtility.PingObject(objectAdded);
-        };
-        
-        // selectionHistory.
-        objectField.SetValueWithoutNotify(objectAdded);
-        
-        _historyObjectsContainer.Add(selectionContainer);
+            // Each editor window contains a root VisualElement object
+            var root = rootVisualElement;
+
+            _historyObjectsContainer = new VisualElement();
+            
+            root.Add(_historyObjectsContainer);
+
+            _styleSheet = LoadStyleSheet();
+            _visualTreeAsset = LoadTreeAsset();
+
+            if (_styleSheet == null || _visualTreeAsset == null)
+            {
+                Debug.LogError("Failed to initialize selection history");
+                return;
+            }
+            
+            root.styleSheets.Add(_styleSheet);
+            
+            var clearButton = new Button(delegate
+            {
+                selectionHistory.Clear();
+                // _historyObjectsContainer.Clear();
+                // clear list too
+            });
+            clearButton.text = "Clear";
+            
+            root.Add(clearButton);
+            
+            var scheduledAction = root.schedule.Execute(() =>
+            {
+                // textFields.ForEach(t => t.value = m_Tank.tankName);
+                // integerFields.ForEach(t => t.value = m_Tank.tankSize);
+            });
+            scheduledAction.Every(100); // ms
+
+            selectionHistory.objectAdded += AddSelectionField;
+            selectionHistory.cleared += () =>
+            {
+                _historyObjectsContainer.Clear();
+            };
+
+            // Selection.selectionChanged += OnSelectionChanged;
+            
+            // regenerate
+    //        _historyObjectsContainer.Clear();
+            
+            selectionHistory.History.ForEach(AddSelectionField);
+            
+        }
+
+        public void OnDisable()
+        {
+            selectionHistory.objectAdded -= AddSelectionField;
+            // Selection.selectionChanged -= OnSelectionChanged;
+        }
+
+        private void AddSelectionField(Object objectAdded)
+        {
+            // if object field with object added already, remove it...
+            
+            var tree = _visualTreeAsset.CloneTree();
+            var selectionContainer = tree.Q("Selection");
+            var objectField = selectionContainer.Q<ObjectField>( );
+
+            var button = selectionContainer.Q<Button>();
+            button.text = "Ping";
+            objectField.pickingMode = PickingMode.Ignore;
+            
+            button.clickable.clicked += delegate
+            {
+                EditorGUIUtility.PingObject(objectAdded);
+            };
+            
+            // selectionContainer.Q<Label>("Dragger").AddManipulator(new HistoryItemDragManipulator(this, objectAdded));
+            
+            objectField.SetValueWithoutNotify(objectAdded);
+            
+            _historyObjectsContainer.Add(selectionContainer);
+        }
     }
 }
