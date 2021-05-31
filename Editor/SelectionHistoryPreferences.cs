b191e85a712592377807e2f4681512ad2803eb8b
Ariel Coppes <ariel.coppes@gmail.com>
2021-05-30 22:16:11 -0300

    just renamed variable for destroyed concept change

--- Editor/SelectionHistoryPreferences.cs
+++ Editor/SelectionHistoryPreferences.cs
@@ -8,7 +8,7 @@ namespace Gemserk {
 
         static int historySize;
 
-        static bool autoremoveDeleted;
+        static bool autoremoveDestroyed;
 
         static bool autoRemoveDuplicated;
 
@@ -29,7 +29,7 @@ namespace Gemserk {
                 guiHandler = (searchContext) => {
                     if (!prefsLoaded) {
                         historySize = EditorPrefs.GetInt(SelectionHistoryWindow.HistorySizePrefKey, defaultHistorySize);
-                        autoremoveDeleted = EditorPrefs.GetBool(SelectionHistoryWindow.HistoryAutomaticRemoveDeletedPrefKey, true);
+                        autoremoveDestroyed = EditorPrefs.GetBool(SelectionHistoryWindow.HistoryAutomaticRemoveDeletedPrefKey, true);
                         autoRemoveDuplicated = EditorPrefs.GetBool(SelectionHistoryWindow.HistoryAllowDuplicatedEntriesPrefKey, false);
                         showHierarchyObjects = EditorPrefs.GetBool(SelectionHistoryWindow.HistoryShowHierarchyObjectsPrefKey, true);
                         showProjectViewObjects = EditorPrefs.GetBool(SelectionHistoryWindow.HistoryShowProjectViewObjectsPrefKey, true);
@@ -42,7 +42,7 @@ namespace Gemserk {
                     }
 
                     historySize = EditorGUILayout.IntField("History Size", historySize);
-                    autoremoveDeleted = EditorGUILayout.Toggle("Auto Remove Unreferenced", autoremoveDeleted);
+                    autoremoveDestroyed = EditorGUILayout.Toggle("Auto Remove Destroyed", autoremoveDestroyed);
                     autoRemoveDuplicated = EditorGUILayout.Toggle("Allow duplicated entries", autoRemoveDuplicated);
                     showHierarchyObjects = EditorGUILayout.Toggle("Show HierarchyView objects", showHierarchyObjects);
                     showProjectViewObjects = EditorGUILayout.Toggle("Show ProjectView objects", showProjectViewObjects);
@@ -62,7 +62,7 @@ namespace Gemserk {
 
                     if (GUI.changed) {
                         EditorPrefs.SetInt(SelectionHistoryWindow.HistorySizePrefKey, historySize);
-                        EditorPrefs.SetBool(SelectionHistoryWindow.HistoryAutomaticRemoveDeletedPrefKey, autoremoveDeleted);
+                        EditorPrefs.SetBool(SelectionHistoryWindow.HistoryAutomaticRemoveDeletedPrefKey, autoremoveDestroyed);
                         EditorPrefs.SetBool(SelectionHistoryWindow.HistoryAllowDuplicatedEntriesPrefKey, autoRemoveDuplicated);
 
                         EditorPrefs.SetBool(SelectionHistoryWindow.HistoryShowHierarchyObjectsPrefKey, showHierarchyObjects);
