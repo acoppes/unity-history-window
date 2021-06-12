a3b8fc6fbe6136e0f42dd291a269ba5f70caf45e
Ariel Coppes <ariel.coppes@gmail.com>
2021-06-11 23:18:04 -0300

    optional favorite button in selection hisotry new

--- Assets/Gemserk.SelectionHistory/Editor/UnityBuiltInIcons.cs
+++ Assets/Gemserk.SelectionHistory/Editor/UnityBuiltInIcons.cs
@@ -4,6 +4,7 @@ namespace Gemserk
     {
         public const string pickObjectIconName = "d_pick";
         public const string favoriteIconName = "d_Favorite Icon";
+        public const string favoriteEmptyIconName = "d_Favorite";
         public const string removeIconName = "d_ol_minus";
         public const string tagIconName = "AssetLabelIcon";
         public const string searchIconName = "d_Search Icon";
