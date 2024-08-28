6d85852eaf8a7b12d70d85d5f3bf24955cf9881e
Ariel Coppes <ariel.coppes@gmail.com>
2024-08-28 14:55:18 -0300

    fix: style reacts better to dark/light theme, had to change a bit the current style but it is almost the same.

--- Editor/UnityBuiltInIcons.cs
+++ Editor/UnityBuiltInIcons.cs
@@ -3,24 +3,24 @@ namespace Gemserk
     public static class UnityBuiltInIcons
     {
         public const string pickObjectIconName = "d_pick";
-        public const string favoriteWindowIconName = "d_Favorite Icon";
+        public const string favoriteWindowIconName = "Favorite Icon";
         
         #if UNITY_2022_3_OR_NEWER
-        public const string favoriteIconName = "d_Favorite_colored";
-        public const string favoriteEmptyIconName = "d_Favorite icon";
+        public const string favoriteIconName = "Favorite_colored";
+        public const string favoriteEmptyIconName = "Favorite icon";
         #else 
-        public const string favoriteIconName = "d_Toolbar Minus";
-        public const string favoriteEmptyIconName = "d_Toolbar Plus";
+        public const string favoriteIconName = "Toolbar Minus";
+        public const string favoriteEmptyIconName = "Toolbar Plus";
         #endif
 
         
-        public const string removeIconName = "d_Toolbar Minus";
+        public const string removeIconName = "Toolbar Minus";
         public const string tagIconName = "AssetLabelIcon";
-        public const string searchIconName = "d_Search Icon";
+        public const string searchIconName = "Search Icon";
         public const string eyeViewToolIconName = "d_ViewToolOrbit";
         public const string refreshIconName = "TreeEditor.Refresh";
 
-        public const string openAssetIconName = "d_FolderOpened Icon";
+        public const string openAssetIconName = "FolderOpened Icon";
         public const string clearSearchToolbarIconName = "d_clear";
         
     }
