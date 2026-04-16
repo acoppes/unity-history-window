bbb1ee0f317ecdc03097b5e458052a79e458c4fc
Ariel Coppes <ariel.coppes@gmail.com>
2026-04-16 09:44:27 -0300

    adjusted package dep to be relative folder not absolute and removed unity2020 project

--- unity2020/Assets/DragExample/Unit.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class Unit : MonoBehaviour {
-
-	public Health health;
-
-}
--- unity2020/releasepackage.sh
+++
@@ -1,3 +0,0 @@
-
-# pushes subtree into specific branch name
-git subtree push --prefix Assets/Gemserk.SelectionHistory origin upm-package-1.0.2
